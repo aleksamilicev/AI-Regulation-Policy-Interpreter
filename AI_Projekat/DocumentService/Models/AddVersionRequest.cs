8bdee3dc07655274296ce43f7ba44efb5f4aad20
aleksamilicev <amilicev92@gmail.com>
2026-02-04 19:45:15 +0100

    Document Service
    Uloga ovog servisa je da čuva dokumente i verzije.
    Koristi Service Fabric Reliable Collections.

---
+++ AI_Projekat/DocumentService/Models/AddVersionRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentService.Models
+{
+    public class AddVersionRequest
+    {
+        public string RawText { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime? ValidTo { get; set; }
+    }
+}
